560cf52a8d627a84020646869068e77d8b7a7fe6
gogbajbobo <grigoriev.maxim@gmail.com>
2017-07-07 15:57:52 +0300

    edit stock_availability.php

--- includes/stock_availability.php
+++ includes/stock_availability.php
@@ -12,6 +12,16 @@ add_filter( 'woocommerce_get_availability', 'custom_override_get_availability', 
 
 // The hook in function $availability is passed via the filter!
 function custom_override_get_availability( $availability, $_product ) {
-    if ( $_product->is_in_stock() ) $availability['availability'] = __('In stock', 'woocommerce');
+
+    // Change In Stock Text
+    if ( $_product->is_in_stock() ) {
+        $availability['availability'] = 'В наличии';
+    }
+    // Change Out of Stock Text
+    if ( ! $_product->is_in_stock() ) {
+        $availability['availability'] = 'Поставка в течение 2-x недель';
+    }
+
     return $availability;
+
 }
