ad8f1ddff3976c6ec6ad85bf021577f35860da28
gogbajbobo <grigoriev.maxim@gmail.com>
2017-07-07 11:36:05 +0300

    comment out subtotal

--- woocommerce/cart/cart-totals.php
+++ woocommerce/cart/cart-totals.php
@@ -29,10 +29,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<table cellspacing="0" class="shop_table shop_table_responsive">
 
-		<tr class="cart-subtotal">
-			<th><?php _e( 'Subtotal', 'woocommerce' ); ?></th>
-			<td data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>"><?php wc_cart_totals_subtotal_html(); ?></td>
-		</tr>
+<!--		<tr class="cart-subtotal">-->
+<!--			<th>--><?php //_e( 'Subtotal', 'woocommerce' ); ?><!--</th>-->
+<!--			<td data-title="--><?php //esc_attr_e( 'Subtotal', 'woocommerce' ); ?><!--">--><?php //wc_cart_totals_subtotal_html(); ?><!--</td>-->
+<!--		</tr>-->
 
 		<?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
 			<tr class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
