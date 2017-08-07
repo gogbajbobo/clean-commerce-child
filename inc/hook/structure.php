4db946c3c566415ac367a1b9a7acf090d47e145b
gogbajbobo <grigoriev.maxim@gmail.com>
2017-08-07 15:25:41 +0300

    comment out <link rel="pingback" />

--- inc/hook/structure.php
+++ inc/hook/structure.php
@@ -12,31 +12,20 @@
      * grimax
      */
 
-if ( ! function_exists( 'clean_commerce_doctype' ) ) :
-	/**
-	 * Doctype Declaration.
-	 *
-	 * @since 1.0.0
-	 */
-	function clean_commerce_doctype() {
-		?><!DOCTYPE html> <html lang="ru-RU"><?php
-	}
-endif;
-
 if ( ! function_exists( 'clean_commerce_head' ) ) :
-	/**
-	 * Header Codes.
-	 *
-	 * @since 1.0.0
-	 */
-	function clean_commerce_head() {
-		?>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="http://maxbook.local/~grimax/krikuz/xmlrpc.php">
-		<?php
-	}
+    /**
+     * Header Codes.
+     *
+     * @since 1.0.0
+     */
+    function clean_commerce_head() {
+        ?>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--        <link rel="profile" href="http://gmpg.org/xfn/11">-->
+<!--        <link rel="pingback" href="--><?php //bloginfo( 'pingback_url' ); ?><!--">-->
+        <?php
+    }
 endif;
 
 
