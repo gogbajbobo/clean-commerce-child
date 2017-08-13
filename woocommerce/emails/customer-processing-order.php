0547f0f39170dd57c25701385214338ee98fa810
gogbajbobo <grigoriev.maxim@gmail.com>
2017-08-13 14:01:55 +0300

    changes in customer-processing-order.php

--- woocommerce/emails/customer-processing-order.php
+++ woocommerce/emails/customer-processing-order.php
@@ -25,7 +25,14 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
-<p><?php _e( "Your order has been received and is now being processed. Your order details are shown below for your reference:", 'woocommerce' ); ?></p>
+<p>
+    <?php
+
+        echo 'Ваша заявка была принята и сейчас находится в обработке. Информация о заявке приведена ниже:';
+//        _e( "Your order has been received and is now being processed. Your order details are shown below for your reference:", 'woocommerce' );
+
+    ?>
+</p>
 
 <?php
 
