b933a3b0d296c28ec417525f8e921534e093e7e9
gogbajbobo <grigoriev.maxim@gmail.com>
2017-08-13 13:59:18 +0300

    changes in email-order-details.php

--- woocommerce/emails/email-order-details.php
+++ woocommerce/emails/email-order-details.php
@@ -25,9 +25,26 @@ $text_align = is_rtl() ? 'right' : 'left';
 do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
 
 <?php if ( ! $sent_to_admin ) : ?>
-	<h2><?php printf( __( 'Order #%s', 'woocommerce' ), $order->get_order_number() ); ?></h2>
+	<h2>
+        <?php
+
+            printf('Заявка №%s', $order->get_order_number());
+//            printf( __( 'Order #%s', 'woocommerce' ), $order->get_order_number() );
+
+        ?>
+    </h2>
 <?php else : ?>
-	<h2><a class="link" href="<?php echo esc_url( admin_url( 'post.php?post=' . $order->get_id() . '&action=edit' ) ); ?>"><?php printf( __( 'Order #%s', 'woocommerce' ), $order->get_order_number() ); ?></a> (<?php printf( '<time datetime="%s">%s</time>', $order->get_date_created()->format( 'c' ), wc_format_datetime( $order->get_date_created() ) ); ?>)</h2>
+	<h2>
+        <a class="link" href="<?php echo esc_url( admin_url( 'post.php?post=' . $order->get_id() . '&action=edit' ) ); ?>">
+            <?php
+
+                printf('Заявка №%s', $order->get_order_number());
+//                printf( __( 'Order #%s', 'woocommerce' ), $order->get_order_number() );
+            //
+            ?>
+        </a>
+        (<?php printf( '<time datetime="%s">%s</time>', $order->get_date_created()->format( 'c' ), wc_format_datetime( $order->get_date_created() ) ); ?>)
+    </h2>
 <?php endif; ?>
 
 <table class="td" cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" border="1">
