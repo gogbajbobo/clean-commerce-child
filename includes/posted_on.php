13f2a07aee04c16cc69875f1cd200978a2cc8e2a
gogbajbobo <grigoriev.maxim@gmail.com>
2017-10-11 15:29:32 +0300

    fix hentry missing author error

--- includes/posted_on.php
+++ includes/posted_on.php
@@ -37,7 +37,7 @@ function clean_commerce_posted_on() {
         $byline = sprintf(
             '%s',
 //				'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-            '<span class="author vcard">' . esc_html( get_the_author() ) . '</span>'
+            '<span class="author vcard"><span class="fn n">' . esc_html( get_the_author() ) . '</span></span>'
         );
     }
 
